c383bab91e5e0698c5f609a492fad048ba4bd0b8
Pavel Gorbunov <pavlogorbunov@yandex.ru>
2022-04-10 00:33:40 +0300

    deleteitem done

--- script/index.js
+++ script/index.js
@@ -5,6 +5,7 @@ const addItemBtn = document.querySelector('.profile__info-addbutton');
 const escapeRenameButton = document.querySelectorAll('.popup__form-escape')[0];
 const escapeAddItemButton = document.querySelectorAll('.popup__form-escape')[1];
 const likeButtons = document.querySelectorAll('.cards__card-likebtn');
+const deleteButtons = document.querySelectorAll('.crads__card-trash');
 const renameForm = document.querySelectorAll('.popup__form')[0];
 const addItemForm = document.querySelectorAll('.popup__form')[1];
 
@@ -47,11 +48,16 @@ function addCard(evt) {
     const item = `<div class="cards__card"><div class="cards__card-img-container"><img src="${inputAddItemSrc.value}" alt="" class="cards__card-img"><div class="crads__card-trash"></div></div><div class="cards__card-title">${inputAddItemName.value}<button class="cards__card-likebtn"></button></div></div>`;
     cards.insertAdjacentHTML("afterbegin", item);
     cards.querySelector('.cards__card-likebtn').addEventListener('click',  like);
+    cards.querySelector('.crads__card-trash').addEventListener('click',  deleteItem);
     showOrHidePopupAddItem();
 }
 
 function like(event) {
     event.currentTarget.classList.toggle('cards__card-likebtn_activated');
+}
+
+function deleteItem(event) {
+    event.currentTarget.parentNode.parentNode.remove();
 }
 
 editButton.addEventListener('click', openRenameForm);
@@ -65,4 +71,5 @@ addItemForm.addEventListener('click', event => event.stopPropagation());
 addItemForm.addEventListener('submit', addCard);
 addItemBtn.addEventListener('click', showOrHidePopupAddItem);
 likeButtons.forEach(el => el.addEventListener('click',  like));
+deleteButtons.forEach(el => el.addEventListener('click',  deleteItem));
 //window.addEventListener('keydown', event => console.log(event.code));
