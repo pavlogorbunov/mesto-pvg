1319c3cb0ff496b70d63132a99493ab30285050c
Pavel Gorbunov <pavlogorbunov@yandex.ru>
2022-04-09 20:13:32 +0300

    additem base

--- script/index.js
+++ script/index.js
@@ -1,42 +1,60 @@
-const popup = document.querySelector('.popup');
+const popupRename = document.querySelectorAll('.popup')[0];
+const popupAddItem = document.querySelectorAll('.popup')[1];
 const editButton = document.querySelector('.profile__info-editbutton');
-const escapeButton = document.querySelector('.popup__form-escape');
+const addItemBtn = document.querySelector('.profile__info-addbutton');
+const escapeRenameButton = document.querySelectorAll('.popup__form-escape')[0];
+const escapeAddItemButton = document.querySelectorAll('.popup__form-escape')[1];
 const saveButton = document.querySelector('.popup__form-save');
 const likeButtons = document.querySelectorAll('.cards__card-likebtn');
-const form = document.querySelector('.popup__form');
+const renameForm = document.querySelectorAll('.popup__form')[0];
+const addItemForm = document.querySelectorAll('.popup__form')[1];
 
-const inputs = document.querySelectorAll('.popup__form-input');
+const inputs = popupRename.querySelectorAll('.popup__form-input');
 let inputProfileName = inputs[0];
 let inputProfileOccupation = inputs[1];
 
-function showOrHidePopup() {
-    popup.classList.toggle('popup_visible');
+function showOrHidePopupRename() {
+    popupRename.classList.toggle('popup_visible');
 }
 
-function openForm() {
+function showOrHidePopupAddItem() {
+    popupAddItem.classList.toggle('popup_visible');
+}
+
+function openRenameForm() {
     let currentProfileName = document.querySelector('.profile__info-name').textContent;
     let currentProfileOccupation = document.querySelector('.profile__info-titles').textContent;
     inputProfileName.value = currentProfileName;
     inputProfileOccupation.value = currentProfileOccupation;
-    showOrHidePopup();
+    showOrHidePopupRename();
 }
 
 function renameProfile(evt) {
     evt.preventDefault();
     document.querySelector('.profile__info-name').textContent = inputProfileName.value;
     document.querySelector('.profile__info-titles').textContent = inputProfileOccupation.value;
-    showOrHidePopup();
+    showOrHidePopupRename();
+}
+
+function addCard(evt) {
+    evt.preventDefault();
+    showOrHidePopupAddItem();
 }
 
 function like(event) {
     event.currentTarget.classList.toggle('cards__card-likebtn_activated');
 }
 
-editButton.addEventListener('click', openForm);
-escapeButton.addEventListener('click', showOrHidePopup);
-popup.addEventListener('click', showOrHidePopup);
-form.addEventListener('click', event => event.stopPropagation());
-form.addEventListener('submit', renameProfile);
+editButton.addEventListener('click', openRenameForm);
+escapeRenameButton.addEventListener('click', showOrHidePopupRename);
+escapeAddItemButton.addEventListener('click', showOrHidePopupAddItem);
+popupRename.addEventListener('click', showOrHidePopupRename);
+renameForm.addEventListener('click', event => event.stopPropagation());
+renameForm.addEventListener('submit', renameProfile);
+popupAddItem.addEventListener('click', showOrHidePopupAddItem);
+addItemForm.addEventListener('click', event => event.stopPropagation());
+addItemForm.addEventListener('submit', addCard);
+addItemBtn.addEventListener('click', showOrHidePopupAddItem);
 //saveButton.addEventListener('click', renameProfile);
 likeButtons.forEach(el => el.addEventListener('click',  like));
-window.addEventListener('keydown', event => console.log(event.code));
+//window.addEventListener('keydown', event => console.log(event.code));
