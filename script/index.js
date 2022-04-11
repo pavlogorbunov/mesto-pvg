92a29f510c8b8cec4d38bdd9bbd9f772b612de7e
Pavel Gorbunov <pavlogorbunov@yandex.ru>
2022-04-11 18:41:51 +0300

    fix

--- script/index.js
+++ script/index.js
@@ -1,13 +1,12 @@
 const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__info-editbutton');
 const escapeButton = document.querySelector('.popup__form-escape');
-const saveButton = document.querySelector('.popup__form-save');
-const likeButtons = document.querySelectorAll('.cards__card-likebtn');
+//const likeButtons = document.querySelectorAll('.cards__card-likebtn');
 const form = document.querySelector('.popup__form');
 
 const inputs = document.querySelectorAll('.popup__form-input');
-let inputProfileName = inputs[0];
-let inputProfileOccupation = inputs[1];
+let inputProfileName = popup.querySelector('.popup__form-input_name_name');
+let inputProfileOccupation = popup.querySelector('.popup__form-input_name_occupation');
 
 function showOrHidePopup() {
     popup.classList.toggle('popup_visible');
@@ -28,15 +27,14 @@ function renameProfile(evt) {
     showOrHidePopup();
 }
 
-function like(event) {
+/*function like(event) {
     event.currentTarget.classList.toggle('cards__card-likebtn_activated');
-}
+}*/
 
 editButton.addEventListener('click', openForm);
 escapeButton.addEventListener('click', showOrHidePopup);
-popup.addEventListener('click', showOrHidePopup);
-form.addEventListener('click', event => event.stopPropagation());
+//popup.addEventListener('click', showOrHidePopup);
+//form.addEventListener('click', event => event.stopPropagation());
 form.addEventListener('submit', renameProfile);
-//saveButton.addEventListener('click', renameProfile);
-likeButtons.forEach(el => el.addEventListener('click',  like));
-window.addEventListener('keydown', event => console.log(event.code));
+//likeButtons.forEach(el => el.addEventListener('click',  like));
+//window.addEventListener('keydown', event => console.log(event.code));
